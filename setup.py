378e1010c6db0a22e594e399cf02712546c7665b
josiahwsmith10 <47935482+josiahwsmith10@users.noreply.github.com>
2023-08-27 19:16:55 -0400

    Major commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "complextorch",
+    version = "0.0.1",
+    author = "Josiah W. Smith",
+    author_email = "josiah.radar@gmail.com",
+    description = "A lightweight complex-valued neural network package built on PyTorch",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "package URL",
+    packages=["complextorch"],
+    project_urls = {
+        "Bug Tracker": "package issues URL",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "complextorch"},
+    python_requires = ">=3.6"
+)
