681f2b05d5f39e6225bb67e99a97924df8e58a34
josiahwsmith10 <47935482+josiahwsmith10@users.noreply.github.com>
2023-08-28 15:02:35 -0400

    FFT docs

--- complextorch/nn/modules/fft.py
+++ complextorch/nn/modules/fft.py
@@ -6,7 +6,12 @@ __all__ = ["FFTBlock", "IFFTBlock"]
 
 
 class FFTBlock(nn.Module):
-    """FFT Block."""
+    """
+    FFT Block
+    ---------
+    
+    A complex-valued module that performs the forward fast Fourier transform. 
+    """
 
     def __init__(self, n=None, dim=-1, norm=None) -> None:
         super(FFTBlock, self).__init__()
@@ -16,11 +21,24 @@ class FFTBlock(nn.Module):
         self.norm = norm
 
     def forward(self, x: CVTensor) -> CVTensor:
+        """Performs forward fast Fourier transform (FFT) on the input tensor.
+
+        Args:
+            x (CVTensor): input tensor
+
+        Returns:
+            CVTensor: fft(x) using configuration established on initialization
+        """
         return x.fft(x, n=self.n, dim=self.dim, norm=self.norm)
 
 
 class IFFTBlock(nn.Module):
-    """IFFT Block."""
+    """
+    IFFT Block
+    ----------
+    
+    A complex-valued module that performs the inverse fast Fourier transform. 
+    """
 
     def __init__(self, n=None, dim=-1, norm=None) -> None:
         super(IFFTBlock, self).__init__()
@@ -30,4 +48,12 @@ class IFFTBlock(nn.Module):
         self.norm = norm
 
     def forward(self, x: CVTensor) -> CVTensor:
+        """Performs inverse fast Fourier transform (FFT) on the input tensor.
+
+        Args:
+            x (CVTensor): input tensor
+
+        Returns:
+            CVTensor: ifft(x) using configuration established on initialization
+        """
         return x.ifft(x, n=self.n, dim=self.dim, norm=self.norm)
