d26725ac81357f77d82c02d9dcb797f7073afffb
josiahwsmith10 <47935482+josiahwsmith10@users.noreply.github.com>
2023-08-30 15:17:31 -0400

    Update deployment version

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name = "complextorch",
-    version = "1.0.0",
+    version = "0.1.0",
     author = "Josiah W. Smith",
     author_email = "josiah.radar@gmail.com",
     description = "A lightweight complex-valued neural network package built on PyTorch",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = "package URL",
-    packages=["complextorch"],
+    url="https://www.github.com/josiahwsmith10/complextorch",
+    packages=find_packages(),
     project_urls = {
         "Bug Tracker": "package issues URL",
+        "Documentation": "https://complextorch.readthedocs.io/en/latest/index.html",
+        "GitHub": "https://www.github.com/josiahwsmith10/complextorch",
     },
     classifiers = [
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
-    package_dir = {"": "complextorch"},
+    package_dir = {"": "./"},
     python_requires = ">=3.6"
 )
