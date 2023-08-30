60a56d4399eca2afd333a7699261f535d2e44ed3
josiahwsmith10 <47935482+josiahwsmith10@users.noreply.github.com>
2023-08-30 15:48:28 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setup(
     name = "complextorch",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Josiah W. Smith",
     author_email = "josiah.radar@gmail.com",
     description = "A lightweight complex-valued neural network package built on PyTorch",
