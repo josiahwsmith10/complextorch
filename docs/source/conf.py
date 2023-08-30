18d7a492b83f67bc0710d7be98ea5acfabc0f9e2
josiahwsmith10 <47935482+josiahwsmith10@users.noreply.github.com>
2023-08-30 15:19:35 -0400

    Updated version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'complextorch'
 copyright = '2023, Josiah W. Smith'
 author = 'Josiah W. Smith'
-release = '1.0.0'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
